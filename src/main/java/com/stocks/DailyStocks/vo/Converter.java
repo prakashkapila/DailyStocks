2392519fae98db766458f445d0ea1914a2d0b796
prakashkapila <prakashkapila@yahoo.com>
2022-09-10 19:07:57 -0400

    Sample regression codebase

--- src/main/java/com/stocks/DailyStocks/vo/Converter.java
+++ src/main/java/com/stocks/DailyStocks/vo/Converter.java
@@ -1,9 +1,13 @@
 package com.stocks.DailyStocks.vo;
  
+import java.text.SimpleDateFormat;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.bson.Document;
 
 public class Converter {
-
+	static Logger log = LogManager.getLogger(Converter.class);
 	final static String OP="openPrice";
 	final static String HP="highPrice";
 	final static String LP="lowPrice";
@@ -53,4 +57,40 @@ public class Converter {
 			return vo;
 		 
 	}
+	static String getDay(Date date)
+	{
+		String ret = "";
+		Calendar form = Calendar.getInstance();
+		form.setTime(date);
+		switch(form.get(Calendar.DAY_OF_WEEK))
+		{
+		case Calendar.SUNDAY: return "SUNDAY";
+		case Calendar.SATURDAY: return "SATURDAY";
+ 		case Calendar.MONDAY: return "MONDAY";
+		case Calendar.TUESDAY: return "TUESDAY";
+		case Calendar.WEDNESDAY: return "WEDNESDAY";
+		case Calendar.THURSDAY: return "THURSDAY";
+		case Calendar.FRIDAY: return "FRIDAY";
+		
+		}
+		return ret;
+	}
+	public static PriceVO convertRow(Row setRow ) throws ParseException
+	{
+		PriceVO vo = new PriceVO();
+		System.out.println(setRow.mkString());
+		log.info(setRow.mkString());
+		//vo.setDate(DateFormat.getDateInstance().parse(setRow.getString(0) ));
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		vo.setDate(sdf.parse(setRow.getString(0)));
+		vo.setOpenPrice(Double.parseDouble(setRow.getString(1)));
+		vo.setHighPrice(Double.parseDouble(setRow.getString(2)));
+		vo.setLowPrice(Double.parseDouble(setRow.getString(3)));
+		vo.setClosingPrice(Double.parseDouble(setRow.getString(4)));
+		vo.setVolume(Long.parseLong(setRow.getString(6)));
+		vo.setPercent(((vo.getClosingPrice()-vo.getOpenPrice())/vo.getClosingPrice())*100);
+		vo.setDayMonth(getDay(vo.getDate()));
+		return vo;
+	}
+	
 }
